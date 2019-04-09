0716d82a5e53ecbd4b9c84028fec2f1d739c5380
Andrew <twintrool@gmail.com>
2019-04-08 19:42:42 -0500

    config

--- next.config.js
+++ next.config.js
@@ -1,29 +1,12 @@
 module.exports = {
-  assetPrefix: process.env.NODE_ENV === "production" ? "/{reponame}" : "",
+  // exportPathMap: function() {
+  //   return {
+  //     "/": { page: "/" },
+  //     "/about": { page: "/about" }
+  //   };
+  // },
+  assetPrefix: process.env.NODE_ENV === "production" ? "/wolfPack" : "",
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // // Note: we provide webpack above so you should not `require` it
-    // // Perform customizations to webpack config
-    // // Important: return the modified config
-
-    // // Example using webpack option
-    // //   config.plugins.push(new webpack.IgnorePlugin(//__tests__//));
-    // //   return config;
-    // config.module.rules.push({
-    //   test: /\.(woff|woff2|eot|ttf|otf)$/,
-    //   use: [
-    //     {
-    //       loader: "url-loader",
-    //       options: {
-    //         limit: 8192,
-    //         fallback: "file-loader",
-    //         publicPath: "/_next/static/fonts/",
-    //         outputPath: "static/fonts/",
-    //         name: "[name]-[hash].[ext]"
-    //       }
-    //     }
-    //   ]
-    // });
-    // return config;
     config.module.rules.push({
       test: /\.(png|PNG|jpg|gif|svg|eot|ttf|woff|woff2)$/,
       use: {
